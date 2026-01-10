29377ac3f6179d757ee0504a37746f24f5f42fd6
lasha_shala <lashawrk@gmail.com>
2026-01-10 14:40:39 +0400

    added player class and its movement

--- code/main.py
+++ code/main.py
@@ -1,17 +1,54 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pygame
+from os.path import join
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Player(pygame.sprite.Sprite):
+    def __init__(self, surf, *groups):
+        super().__init__(*groups)
+        self.image = surf
+        self.rect = self.image.get_frect(center=(WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.Vector2()
+        self.speed = 400
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        self.direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+        self.direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+        self.direction = self.direction.normalize() if self.direction else self.direction
+        self.rect.center += self.direction * self.speed * dt
 
 
+pygame.init()
+WINDOW_WIDTH, WINDOW_HEIGHT = 1080, 720
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Asteroids')
+clock = pygame.Clock()
 
-# Press the green button in the gutter to run the script.
+player_surf = pygame.image.load(join('../images', 'player.png')).convert_alpha()
+
+all_sprites = pygame.sprite.Group()
+player = Player(player_surf, all_sprites)
+
+def game_loop():
+    running = True
+
+    while running:
+        dt = clock.tick() / 1000
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        all_sprites.update(dt)
+
+        display_surface.fill('#111111')
+        all_sprites.draw(display_surface)
+
+        pygame.display.update()
+
+    pygame.quit()
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    game_loop()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
